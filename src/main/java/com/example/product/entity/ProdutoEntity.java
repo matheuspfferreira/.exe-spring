12139d832e7a115d5f24c34d07e117e565ee71b8
Matheus Pintor Fernandes Ferreira <mathfer1000@gmail.com>
2026-03-23 22:08:53 -0300

    feat: adicionar entidades de gerenciamento de produtos

---
+++ src/main/java/com/example/product/entity/ProdutoEntity.java
@@ -0,0 +1,58 @@
+package com.example.product.entity;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "tb_produto")
+public class ProdutoEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "preco")
+    private BigDecimal preco;
+
+    @Column(name = "estoque")
+    private Integer estoque;
+
+    public ProdutoEntity() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public Integer getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(Integer estoque) {
+        this.estoque = estoque;
+    }
+}
---
+++ src/main/java/com/example/product/repository/ProdutoRepository.java
@@ -0,0 +1,6 @@
+package com.example.product.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository {
+}
